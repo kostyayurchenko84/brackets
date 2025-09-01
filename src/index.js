bdbe847f0483ef383be5478a2214f8f25160544b
kostyayurchenko84 <kostyayurchenko84@yandex.ru>
2025-09-01 23:12:38 +0300

    some errors need to be correct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+
+  const bracketPairs = {};
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    const open = bracketsConfig[i][0];
+    const close = bracketsConfig[i][1];
+    bracketPairs[open] = close;
+  }
+
+  const validBrackets = new Set();
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    const open = bracketsConfig[i][0];
+    const close = bracketsConfig[i][1];
+    validBrackets.add(open);
+    validBrackets.add(close);
+  }
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (validBrackets.has(char)) {
+      if (char in bracketPairs) {
+        if (bracketPairs[char] !== char) {
+          stack.push(char);
+        } else if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      }
+    } else {
+      if (stack.length === 0) return false;
+
+      const lastIn = stack.pop();
+      if (bracketPairs[lastIn] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
